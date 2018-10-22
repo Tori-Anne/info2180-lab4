9102a8ebd866e807273fc4145be2ddb1f4792f5d
Tori-Anne <connell_tori@yahoo.com>
2018-10-22 17:57:03 -0500

    exercise 1 attempt 1

--- maze.js
+++ maze.js
@@ -1,1 +1,9 @@
-provides all event handling and behaviour
+//provides all event handling and behaviour to make maze work
+
+
+window.onload = function() {
+	var top_left_wall = document.getElementById("boundary1");
+	top_left_wall.onmouseover = function() {
+		top_left_wall.setAttribute("youlose");
+	}
+}
