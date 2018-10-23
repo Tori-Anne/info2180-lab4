3ed17ebd7aa848a502a11a77818f600015fcf54f
Tori-Anne <connell_tori@yahoo.com>
2018-10-23 01:36:25 -0500

    event listener update

--- maze.js
+++ maze.js
@@ -1,47 +1,55 @@
 //provides all event handling and behaviour to make maze work
 
+var loser = null;
+
 window.onload = function() {
 	var top_left_wall = document.getElementById("boundary1");
-	top_left_wall.onmouseover = function() {
-		top_left_wall.setAttribute("youlose");
-	}
+	top_left_wall.addEventListener("mouseover", function() {
+		top_left_wall.classList.add("youlose");
+	});
 	
 	
 	
 	var all_boundaries = document.querySelectorAll("div#maze div.boundary");
 	for (var i = 0; i < all_boundaries.length; i++) {
-		all_boundaries[i].onmouseover = function() {
-			all_boundaries[i].setAttribute("youlose");
-			document.getElementById("status").innerHTML = "You lose"!;
-		}
+		all_boundaries[i].addEventListener("mouseover", function() {
+			if (loser == false) {
+				loser = true;	
+				all_boundaries[i].classList.add("youlose");
+				document.getElementById("status").innerHTML = "You lose!";
+			}
+		});
 	}
 
-	
+
 	
 	var end = document.getElementById("end");
-	end.onmouseover = function() {
-		document.getElementById("status").innerHTML = "You win!";
-	}
+	end.addEventListener("onmouseover", function() {
+		if (loser == false) {
+			document.getElementById("status").innerHTML = "You win!";
+		}
+	});
 	
+    
 	
-	
-	var start = document.getElementById{"start");
-	start.onclick = function() {
+	var start = document.getElementById("start");
+	start.addEventListener("onclick", function() {
+		loser = false;
 		var all_boundaries = document.querySelectorAll("div#maze div.boundary");
 		for (var i = 0; i < all_boundaries.length; i++) {
 			all_boundaries[i].classList.remove("youlose");
 		}
-	}
+	});
 	
 	
 	
-	var cheat = document.getElementById{"start");
-	cheat.onmouseleave = function() {
+	var cheat = document.getElementById("start");
+	cheat.addEventListener("onmouseleave", function() {
 		for (var i = 0; i < all_boundaries.length; i++) {
-			all_boundaries[i].setAttribute("youlose");
+			all_boundaries[i].classList.add("youlose");
 			document.getElementById("status").innerHTML = "You win!";
 		}
-	}
+	});
 		
 	
 	
